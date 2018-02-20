40d8d07cb4fc4f75f5f578957ad798c119e3bb91
Никита Маслов <nikitamslv@yandex.ru>
2018-02-21 00:49:48 +0300

    Added type for UpdateStep

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 
 
-
 namespace cds {
     namespace container {
 
@@ -16,61 +15,70 @@ namespace cds {
         public:
             int k;
 
-            enum {type_node, type_leaf, type_internal};
+            enum {
+                type_node, type_leaf, type_internal
+            };
+            enum {
+                type_update_state, type_clean, type_prune, type_replace, type_mark
+            };
 
-            struct UpdateStep{};
+            struct UpdateStep {
+                int type = type_update_state;
+            };
 
-            struct Clean: public UpdateStep{};
+            struct Clean : public UpdateStep {
+                Clean():type(type_clean){};
+            };
 
             struct Node {
-                int * keys;
+                int *keys;
                 int type;
                 int number_of_keys;
 
-                Node(int number_of_keys){
+                Node(int number_of_keys) {
                     keys = new int[number_of_keys];
                     type = type_node;
                     this->number_of_keys = number_of_keys;
                 };
 
-                ~Node(){
+                ~Node() {
                     if (keys) {
-                        delete [] keys;
+                        delete[] keys;
                     }
                 }
 
-                virtual void * some_func(){};
+                virtual void *some_func() {};
             };
 
             struct Leaf : public Node {
                 int keyCount;
 
-                Leaf(int keyCount, int number_of_keys):Node(number_of_keys) {
+                Leaf(int keyCount, int number_of_keys) : Node(number_of_keys) {
                     this->keyCount = keyCount;
                     type = type_leaf;
                 }
             };
 
             struct InternalNode : public Node {
-                Node ** c_nodes;
+                Node **c_nodes;
                 UpdateStep pending;
 
-                InternalNode(int number_of_keys):Node(number_of_keys){
+                InternalNode(int number_of_keys) : Node(number_of_keys) {
                     pending = Clean();
-                    c_nodes = new Node*[number_of_keys+1];
+                    c_nodes = new Node *[number_of_keys + 1];
                     type = type_internal;
                 };
 
-                ~InternalNode(){
-                    if (c_nodes){
-                        delete [] c_nodes;
+                ~InternalNode() {
+                    if (c_nodes) {
+                        delete[] c_nodes;
                     }
                 }
 
-                Node * get_c_node(int key,int & pindex){
+                Node *get_c_node(int key, int &pindex) {
                     int i = 0;
                     for (i; i < number_of_keys; ++i) {
-                        if(key < this->keys[i]){
+                        if (key < this->keys[i]) {
                             pindex = i;
                             return this->c_nodes[i];
                         }
@@ -80,24 +88,58 @@ namespace cds {
                 }
             };
 
-            struct ReplaceFlag: public UpdateStep{
-                const Node l;
-                const Node p;
-                const Node newChild;
-                const int pindex;
+            struct ReplaceFlag : public UpdateStep {
+                Node * l;
+                Node * p;
+                Node * newChild;
+                int pindex;
+
+                ReplaceFlag(Node * l, Node * p, Node * newChild, int pindex):type(type_replace){
+                    this->l = l;
+                    this->p = p;
+                    this->newChild = newChild;
+                    this->pindex = pindex;
+                };
+
+                ~ReplaceFlag(){
+                    if(this->l) {delete this->l;}
+                    if(this->p) {delete this->p;}
+                    if(this->newChild) {delete this->newChild;}
+                }
             };
 
-            struct PruneFlag: public UpdateStep{
-                const Node l;
-                const Node p;
-                const Node gp;
-                const UpdateStep ppending;
-                const int gpindex;
+            struct PruneFlag : public UpdateStep {
+                Node * l;
+                Node * p;
+                Node * gp;
+                UpdateStep * ppending;
+                int gpindex;
+
+                PruneFlag(Node * l, Node * p, Node * gp, UpdateStep * ppending):type(type_prune){
+                    this->l = l;
+                    this->p = p;
+                    this->gp = gp;
+                    this->ppending = ppending;
+                };
+
+                ~PruneFlag(){
+                    if (this->l) {delete this->l;}
+                    if (this->p) {delete this->p;}
+                    if (this->gp) {delete this->gp;}
+                }
             };
 
 
-            struct Mark: public UpdateStep{
-                const PruneFlag pending;
+            struct Mark : public UpdateStep {
+                PruneFlag * pending;
+
+                Mark(PruneFlag * pending):type(type_mark){
+                    this->pending = pending;
+                };
+
+                ~Mark(){
+                    if (this->pending) {delete this->pending;}
+                }
             };
 
 
@@ -105,9 +147,8 @@ namespace cds {
         protected:
 
 
-
         public:
-            BrownHelgaKtree():BrownHelgaKtree(3) {
+            BrownHelgaKtree() : BrownHelgaKtree(3) {
 
 
             }
@@ -124,13 +165,13 @@ namespace cds {
                 }
 
 
-                for (int i = 0; i < k+1; ++i) {
-                    leftmostChild->c_nodes[i] = new Leaf(0,k);
+                for (int i = 0; i < k + 1; ++i) {
+                    leftmostChild->c_nodes[i] = new Leaf(0, k);
                 }
 
                 root->c_nodes[0] = leftmostChild;
 
-                for (int i = 1; i < k+1; ++i) {
+                for (int i = 1; i < k + 1; ++i) {
                     root->c_nodes[i] = new Leaf(0, k);
                 }
             }
@@ -141,17 +182,17 @@ namespace cds {
                 }
             }
 
-            struct SearchResult{
-                Node * gparent;
-                Node * parent;
-                Node * leaf;
-                UpdateStep * ppending;
-                UpdateStep * gppending;
+            struct SearchResult {
+                Node *gparent;
+                Node *parent;
+                Node *leaf;
+                UpdateStep *ppending;
+                UpdateStep *gppending;
                 int pindex;
                 int gpindex;
 
-                SearchResult(Node * _gparent, Node * _parent, Node * _leaf, UpdateStep * _ppending,
-                             UpdateStep * _gppending, int _pindex, int _gpindex){
+                SearchResult(Node *_gparent, Node *_parent, Node *_leaf, UpdateStep *_ppending,
+                             UpdateStep *_gppending, int _pindex, int _gpindex) {
                     this->parent = _parent;
                     this->gparent = _gparent;
                     this->leaf = _leaf;
@@ -162,31 +203,31 @@ namespace cds {
                 };
 
                 ~SearchResult() {
-                    if (gparent) { delete gparent;}
-                    if (parent) { delete parent;}
-                    if (leaf) { delete leaf;}
-                    if (ppending) { delete ppending;}
-                    if (gppending) {delete gppending;}
+                    if (gparent) { delete gparent; }
+                    if (parent) { delete parent; }
+                    if (leaf) { delete leaf; }
+                    if (ppending) { delete ppending; }
+                    if (gppending) { delete gppending; }
                 }
             };
 
-            SearchResult * search(int key){
-                Node * gparent = root;
-                Node * parent = root;
-                Node * leaf = dynamic_cast<InternalNode*>(parent)->c_nodes[0];
-                UpdateStep * gppending = &(dynamic_cast<InternalNode*>(parent)->pending);
-                UpdateStep * ppending = &(dynamic_cast<InternalNode*>(parent)->pending);
+            SearchResult *search(int key) {
+                Node *gparent = root;
+                Node *parent = root;
+                Node *leaf = dynamic_cast<InternalNode *>(parent)->c_nodes[0];
+                UpdateStep *gppending = &(dynamic_cast<InternalNode *>(parent)->pending);
+                UpdateStep *ppending = &(dynamic_cast<InternalNode *>(parent)->pending);
                 int gpindex = 1;
                 int pindex = 1;
-                while (leaf->type == type_internal){
+                while (leaf->type == type_internal) {
                     gparent = parent;
                     gppending = ppending;
                     parent = leaf;
-                    ppending = &(dynamic_cast<InternalNode*>(parent)->pending);
+                    ppending = &(dynamic_cast<InternalNode *>(parent)->pending);
                     gpindex = pindex;
-                    leaf = dynamic_cast<InternalNode*>(parent)->get_c_node(key,pindex);
+                    leaf = dynamic_cast<InternalNode *>(parent)->get_c_node(key, pindex);
                 }
-                return new SearchResult(gparent,parent,leaf,ppending,gppending,pindex,gpindex);
+                return new SearchResult(gparent, parent, leaf, ppending, gppending, pindex, gpindex);
             }
 
         };
@@ -196,7 +237,7 @@ namespace cds {
 
 int main() {
     auto *brownHelgaKtree = new cds::container::BrownHelgaKtree();
-    auto * result = brownHelgaKtree->search(1);
+    auto *result = brownHelgaKtree->search(1);
     return 0;
 //    std::cout <<NULL;
 }
